d4bede412204ec1904abd281896b7e9281a4e95b
Kazeem Oluwatimilehin <oluwatimilehinkazeem43@gmail.com>
2025-11-25 10:22:11 +0100

    working on the filter file

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
+import { showNoResult } from "./filter.js";
 import { loadSong, playMusic, pauseMusic } from "./index.js";
 import { defaultLoadSong, defaultPlayMusic, defaultPauseMusic } from "./playlist.js";
+showNoResult()
 
 defaultLoadSong(index), defaultPlayMusic(), defaultPauseMusic()
 
